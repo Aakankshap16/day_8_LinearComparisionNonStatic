3dcebdac59b84476c6b8e3fe415d53586c4db45a
Aakankshap16 <125583605+Aakankshap16@users.noreply.github.com>
2023-03-18 13:32:30 +0530

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -5,8 +5,7 @@
         public static void Main(string[] args) 
         { 
         Console.WriteLine("Welcome to Line Comparison Computation Program");
-            CalculateLength obj = new CalculateLength();
-            obj.check();
+            
         }
     }
 }
