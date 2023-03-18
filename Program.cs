711bb757081eff0becf4acf7866013e764bf9d15
Aakankshap16 <aakankshap971@gmail.com>
2023-03-18 13:44:00 +0530

    with obj call UC2

--- Program.cs
+++ Program.cs
@@ -3,7 +3,7 @@
     class Program
     {
         public static void Main(string[] args) 
-        { 
+        { //check both length
         Console.WriteLine("Welcome to Line Comparison Computation Program");
             ComparisionLength obj = new ComparisionLength();
             obj.check();
