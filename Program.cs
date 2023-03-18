17e711d42337312e293570b1b51df6e21bc89d03
Aakankshap16 <aakankshap971@gmail.com>
2023-03-18 13:24:38 +0530

    program file

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿namespace LineComparisionWithUseObj
+{
+    class Program
+    {
+        public static void Main(string[] args) 
+        { 
+        Console.WriteLine("Welcome to Line Comparison Computation Program");
+            CalculateLength obj = new CalculateLength();
+            obj.check();
+        }
+    }
+}
